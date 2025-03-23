70fc732f1114afa519a91e662fa16194567c9496
Home <calle.carolina@gmail.com>
2025-03-23 09:10:32 +0100

    feat(OrderBookLastValue): add OrderBookLastValue component

---
+++ src/app/components/OrderBookLastValue.tsx
@@ -0,0 +1,8 @@
+export default function OrderBookLastValue() {
+  return (
+    <div className="grid grid-cols-2 h-10">
+      <div className="text-2xl font-bold my-auto">2,004.36</div>
+      <div className="font-bold my-auto">$2,004.36</div>
+    </div>
+  );
+}
