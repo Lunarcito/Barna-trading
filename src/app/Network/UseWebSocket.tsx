d140577649a1317771119d37fecb61b0f041fed7
Home <calle.carolina@gmail.com>
2025-03-23 18:38:24 +0100

    feat(network): add UseWebSocket.tsx for managing WebSocket connections

---
+++ src/app/Network/UseWebSocket.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useWebSocket = (
+  url: string,
+  handler: (data: { [key: string]: string }) => void
+) => {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  useEffect(() => {
+    const webSocket = new WebSocket(url);
+    setSocket(webSocket);
+
+    webSocket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      handler(data);
+    };
+
+    return () => {
+      if (webSocket) {
+        webSocket.close();
+      }
+    };
+  }, [url, handler]);
+
+  return socket;
+};
+
+export default useWebSocket;
