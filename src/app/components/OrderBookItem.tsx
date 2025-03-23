f6b2bd044394a52482712aa23cd462e7360a6895
Home <calle.carolina@gmail.com>
2025-03-23 09:08:09 +0100

    feat(OrderBookItem): add OrderBookItem component

---
+++ src/app/components/OrderBookItem.tsx
@@ -0,0 +1,9 @@
+export default function OrderBookItem() {
+  return (
+    <div className="grid grid-cols-3 h-7">
+      <div>2004.53</div>
+      <div className="text-right">9.1953</div>
+      <div className="text-right">18.43K</div>
+    </div>
+  );
+}
