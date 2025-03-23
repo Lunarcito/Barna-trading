e5d01b1f4f197a386e0d34f6d3c4cd3f16da4e6b
Home <calle.carolina@gmail.com>
2025-03-23 14:51:02 +0100

    chore(OHLCV): import DropdownMenu component

--- src/app/components/OHLCV.tsx
+++ src/app/components/OHLCV.tsx
@@ -1,14 +1,16 @@
+import DropdownMenu from "./DropdownMenu";
 export default function OHLCV() {
   return (
     <div>
-      <div className="grid sm:grid-cols-1 md:grid-cols-6 p-4 gap-4">
-        <div>
-          <div className="grid text-2xl font-bold">
-            <div>BTC/USDT</div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-6 p-2 gap-2">
+        <div className="col-span-1">
+          <div className="text-2xl font-bold mr-4">BTC/USDT</div>
+          <div className="flex items-center gap-2">
+            <div className="text-sm ">Bitcoin Price</div>
+            <DropdownMenu />
           </div>
-          <div>Bitcoin Price</div>
         </div>
-        <div>
+        <div className="col-span-1 flex flex-col justify-start">
           <div className="text-2xl font-bold">1,988.77</div>
           <div className="font-bold">$1,988.77</div>
         </div>
