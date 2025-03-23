292202569f16623c70fdf42a1d027b75b8d18301
Home <calle.carolina@gmail.com>
2025-03-23 21:14:59 +0100

    feat: update OHLCV component to display formatted data and add DropdownMenu

--- src/app/components/OHLCV.tsx
+++ src/app/components/OHLCV.tsx
@@ -1,36 +1,46 @@
+import React from "react";
+import { OHLCVType } from "./OHLCVType";
 import DropdownMenu from "./DropdownMenu";
-export default function OHLCV() {
+import { formatMoney, formatPercentage } from "../utils/Formatter";
+
+const OHLCV: React.FC<OHLCVType> = ({ data }) => {
   return (
     <div>
       <div className="grid sm:grid-cols-1 md:grid-cols-6 p-2 gap-2">
         <div className="col-span-1">
           <div className="text-2xl font-bold mr-4">BTC/USDT</div>
           <div className="flex items-center gap-2">
-            <div className="text-sm ">Bitcoin Price</div>
+            <div className="text-sm">Bitcoin Price</div>
             <DropdownMenu />
           </div>
         </div>
         <div className="col-span-1 flex flex-col justify-start">
-          <div className="text-2xl font-bold">1,988.77</div>
-          <div className="font-bold">$1,988.77</div>
+          <div className="text-2xl font-bold">
+            {formatMoney(data?.lastPrice)}
+          </div>
+          <div className="font-bold">
+            {formatMoney(data?.priceChange) + " " + formatPercentage(data?.priceChangePercent)}
+          </div>
         </div>
         <div className="my-auto">
           <div>24h High</div>
-          <div className="font-bold">84,584.00</div>
+          <div className="font-bold">{formatMoney(data?.highPrice)}</div>
         </div>
         <div className="my-auto">
           <div>24h Low</div>
-          <div className="font-bold">83,800.00</div>
+          <div className="font-bold">{formatMoney(data?.lowPrice)}</div>
         </div>
         <div className="my-auto">
           <div>24h Volume(BTC)</div>
-          <div className="font-bold">83,800.00</div>
+          <div className="font-bold">{formatMoney(data?.tradedBaseVolume)}</div>
         </div>
         <div className="my-auto">
           <div>24h Volume(BTC)</div>
-          <div className="font-bold">83,800.00</div>
+          <div className="font-bold">{formatMoney(data?.totalTradedAssetVolume)}</div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default OHLCV;
