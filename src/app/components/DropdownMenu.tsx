ecf3ea44598e37a154c06e62132617132c841de1
Home <calle.carolina@gmail.com>
2025-03-23 14:49:43 +0100

    style(DropdownMenu): update styles for the dropdown button

--- src/app/components/DropdownMenu.tsx
+++ src/app/components/DropdownMenu.tsx
@@ -18,19 +18,17 @@ export default function DropdownMenu() {
         <div>
           <button
             type="button"
-            className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50"
+            className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-2 py-1 text-xs font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50"
             id="menu-button"
             aria-expanded={isOpen ? "true" : "false"}
             aria-haspopup="true"
             onClick={toggleDropdown}
           >
-            Options
             <svg
-              className="-mr-1 size-5 text-gray-400"
+              className="-mr-1 h-4 w-4 text-gray-400"
               viewBox="0 0 20 20"
               fill="currentColor"
               aria-hidden="true"
-              data-slot="icon"
             >
               <path
                 fillRule="evenodd"
@@ -42,7 +40,7 @@ export default function DropdownMenu() {
         </div>
         {isOpen && (
           <div
-            className="absolute right-0 z-10 mt-2 w-full max-w-[16rem]  origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 focus:outline-hidden"
+            className="absolute right-0 z-10 mt-2 w-auto min-w-max max-w-[16rem]  origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 focus:outline-hidden"
             role="menu"
             aria-orientation="vertical"
             aria-labelledby="menu-button"
@@ -51,7 +49,7 @@ export default function DropdownMenu() {
             <div className="py-1" role="none">
               <a
                 href="#"
-                className="block px-4 py-2 text-sm text-gray-700"
+                className="block px-4 py-2 text-xs text-gray-700"
                 role="menuitem"
                 tabIndex={-1}
                 id="menu-item-0"
@@ -61,7 +59,7 @@ export default function DropdownMenu() {
               </a>
               <a
                 href="#"
-                className="block px-4 py-2 text-sm text-gray-700"
+                className="block px-4 py-2 text-xs text-gray-700"
                 role="menuitem"
                 tabIndex={-1}
                 id="menu-item-1"
@@ -71,7 +69,7 @@ export default function DropdownMenu() {
               </a>
               <a
                 href="#"
-                className="block px-4 py-2 text-sm text-gray-700"
+                className="block px-4 py-2 text-xs text-gray-700"
                 role="menuitem"
                 tabIndex={-1}
                 id="menu-item-2"
@@ -82,7 +80,7 @@ export default function DropdownMenu() {
               <form method="POST" action="#" role="none">
                 <button
                   type="submit"
-                  className="block w-full px-4 py-2 text-left text-sm text-gray-700"
+                  className="block w-full px-4 py-2 text-left text-xs text-gray-700"
                   role="menuitem"
                   tabIndex={-1}
                   id="menu-item-3"
