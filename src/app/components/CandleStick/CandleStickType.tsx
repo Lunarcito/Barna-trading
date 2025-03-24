7d37f023d7b850b0c15c66e0901b4bcd5578867f
Home <calle.carolina@gmail.com>
2025-03-24 11:17:15 +0100

    feat(CandleStick): add CandleStickType interface

---
+++ src/app/components/CandleStick/CandleStickType.tsx
@@ -0,0 +1,18 @@
+export interface CandlestickType {
+  klines: KlineType[];
+}
+
+export interface KlineType {
+  close: number;
+  high: number;
+  low: number;
+  open: number;
+  time: string;
+}
+
+export function convertDate(date: string): string {
+  const dateObject = new Date(date);
+  const monthValue = String(dateObject.getUTCMonth() + 1).padStart(2, "0");
+  const monthDay = String(dateObject.getUTCDate()).padStart(2, "0");
+  return `${dateObject.getUTCFullYear()}-${monthValue}-${monthDay}`;
+}
