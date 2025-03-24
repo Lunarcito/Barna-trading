760c9719c469761baac8b136f4ef15a9ed98de65
Home <calle.carolina@gmail.com>
2025-03-24 09:08:09 +0100

    feat(Orderbook): add OrderBookType and createOrderBook function

---
+++ src/app/components/OrderBook/OrderBookType.tsx
@@ -0,0 +1,18 @@
+import { createOrderBookItem, OrderBookItemType } from "./OrderBookItemType";
+import { TradingPairType } from "../../Models/TradingPairType"
+export interface OrderBookType {
+  bids: OrderBookItemType[];
+  asks: OrderBookItemType[];
+  tradingPair: TradingPairType;
+  lastPrice: string;
+}
+
+export function createOrderBook(input: { [key: string]: any },tradingPair:TradingPairType, lastPrice:string): OrderBookType {
+  const asksItems = input["a"].map((data) => {
+    return createOrderBookItem(data);
+  });
+  const bidsItems = input["b"].map((data) => {
+    return createOrderBookItem(data);
+  });
+  return { bids: bidsItems.slice(0, 12), asks: asksItems.slice(0, 12), tradingPair: tradingPair, lastPrice: lastPrice};
+}
