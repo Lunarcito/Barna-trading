9dc89e79eda2a394ffae9a25bae386e8eaed415d
Home <calle.carolina@gmail.com>
2025-03-24 10:58:27 +0100

    feat(OHLCV): update OHLCV component to display dynamic trading pair info

--- src/app/components/OHLCV/OHLCV.tsx
+++ src/app/components/OHLCV/OHLCV.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import { OHLCVType } from "./OHLCVType";
-import DropdownMenu from "../DropdownMenu/DropdownMenu";
 import { formatMoney, formatPercentage } from "../../utils/Formatter";
 
-const OHLCV: React.FC<OHLCVType> = ({ data }) => {
+const OHLCV: React.FC<OHLCVType> = ({ data, tradingPair }) => {
   return (
     <div>
       <div className="grid sm:grid-cols-1 md:grid-cols-6 p-2 gap-2">
         <div className="col-span-1">
-          <div className="text-2xl font-bold mr-4">BTC/USDT</div>
+          <div className="text-2xl font-bold mr-4">{ tradingPair.baseCurrency+"/"+tradingPair.exchangeCurrency }</div>
           <div className="flex items-center gap-2">
-            <div className="text-sm text-gray-400">Bitcoin Price</div>
-            <DropdownMenu />
+            <div className="text-sm text-gray-400">{ tradingPair.baseCurrencyName } Price</div>
           </div>
         </div>
         <div className="col-span-1 flex flex-col justify-start">
@@ -31,11 +29,11 @@ const OHLCV: React.FC<OHLCVType> = ({ data }) => {
           <div className="font-bold">{formatMoney(data?.lowPrice)}</div>
         </div>
         <div className="my-auto">
-          <div className="text-sm text-gray-400">24h Volume(BTC)</div>
+          <div className="text-sm text-gray-400">24h Volume({ tradingPair.baseCurrency })</div>
           <div className="font-bold">{formatMoney(data?.tradedBaseVolume)}</div>
         </div>
         <div className="my-auto">
-          <div className="text-sm text-gray-400">24h Volume(USDT)</div>
+          <div className="text-sm text-gray-400">24h Volume({ tradingPair.exchangeCurrency })</div>
           <div className="font-bold">{formatMoney(data?.totalTradedAssetVolume)}</div>
         </div>
       </div>
