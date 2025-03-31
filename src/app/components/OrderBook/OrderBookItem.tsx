913284868cacf619813e35860f3c12bd5cf30fe0
Home <calle.carolina@gmail.com>
2025-03-31 23:44:00 +0200

    test(OrderBookItem): add data-testid attributes for testing

--- src/app/components/OrderBook/OrderBookItem.tsx
+++ src/app/components/OrderBook/OrderBookItem.tsx
@@ -1,7 +1,7 @@
 import { OrderBookItemType } from "./OrderBookItemType";
 import { formatDecimal, formatMoney } from "../../utils/Formatter";
 
-function calculateTotal(price: string, quantity: string) {
+export function calculateTotal(price: string, quantity: string) {
   if (quantity != null && price != null) {
     const result = Number(quantity) * Number(price);
     return formatMoney(String(result));
@@ -12,9 +12,9 @@ function calculateTotal(price: string, quantity: string) {
 const OrderBookItem: React.FC<OrderBookItemType> = ({ price, quantity }) => {
   return (
     <div className="grid grid-cols-3 h-7">
-      <div className="text-center text-sm">{ formatMoney(price) }</div>
-      <div className="text-center text-sm">{ formatDecimal(quantity) }</div>
-      <div className="text-right text-sm">{ calculateTotal(price, quantity) }</div>
+      <div data-testid="price"  className="text-center text-sm">{ formatMoney(price) }</div>
+      <div data-testid="quantity"  className="text-center text-sm">{ formatDecimal(quantity) }</div>
+      <div data-testid="total" className="text-right text-sm">{ calculateTotal(price, quantity) }</div>
     </div>
   );
 };
