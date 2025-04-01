b1fe6cbc3bcda1157ee5409b578a140f6f927fea
Home <calle.carolina@gmail.com>
2025-04-01 23:10:12 +0200

    test(OrderBookLastValue): add unit test for OrderBookLastValue component

---
+++ src/app/components/_tests_/OrderBookLastValue.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import OrderBookLastValue from '../OrderBook/OrderBookLastValue';
+import { formatMoney } from '../../utils/Formatter';
+
+jest.mock('../../utils/Formatter', () => ({
+  formatMoney: jest.fn(),
+}));
+
+describe('OrderBookLastValue', () => {
+  it('debería renderizar el valor formateado correctamente', () => {
+    const mockValue = '1000';
+    const formattedValue = '$1,000.00';
+
+    formatMoney.mockReturnValue(formattedValue);
+
+    render(<OrderBookLastValue value={mockValue} />);
+
+    expect(formatMoney).toHaveBeenCalledWith(mockValue);
+
+    expect(screen.getAllByText(formattedValue).length).toBe(2);
+
+    const divs = screen.getAllByText(formattedValue);
+    expect(divs[0]).toHaveTextContent(formattedValue);
+    expect(divs[1]).toHaveTextContent(formattedValue);
+  });
+});
