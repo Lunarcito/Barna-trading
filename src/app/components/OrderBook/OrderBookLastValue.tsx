875a2982915d650365ee65ddd9a9ffaf3bde1c0a
Home <calle.carolina@gmail.com>
2025-05-02 23:28:25 +0200

    style(orderbook): format JSX for readability in OrderBookLastValue

--- src/app/components/OrderBook/OrderBookLastValue.tsx
+++ src/app/components/OrderBook/OrderBookLastValue.tsx
@@ -4,8 +4,12 @@ import { formatMoney } from '../../utils/Formatter';
 const OrderBookLastValue: React.FC<{ value: string }> = ({ value }) => {
   return (
     <div className="grid grid-cols-2 h-10">
-      <div data-testid="last-value-a" className="text-2xl font-bold my-auto">{formatMoney(value)}</div>
-      <div data-testid="last-value-b" className="font-bold my-auto">{formatMoney(value)}</div>
+      <div data-testid="last-value-a" className="text-2xl font-bold my-auto">
+        {formatMoney(value)}
+      </div>
+      <div data-testid="last-value-b" className="font-bold my-auto">
+        {formatMoney(value)}
+      </div>
     </div>
   );
 };
